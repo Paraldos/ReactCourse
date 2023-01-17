563e7fd9144fd115613d0fae4335b15331728077
Lil Cthulhu <54956252+Paraldos@users.noreply.github.com>
2023-01-17 09:55:03 +0100

    new chapter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import React, { useState, useEffect } from "react";
-
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, []);
-
-  async function fetchMoviesHandler() {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        titel: movieData.titel,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
-  }
-
   return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies...</p>}
-        {isLoading && <p>Is Loading...</p>}
-      </section>
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
